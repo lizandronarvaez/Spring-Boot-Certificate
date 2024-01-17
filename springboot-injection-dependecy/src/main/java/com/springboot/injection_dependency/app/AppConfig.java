61055a546a3cc28288f7337562142fc044430647
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-01-17 11:01:47 +0100

    Tema: Inyecciones de dependeicas con @AutoWired, Constructores y Getter y Setters

--- springboot-injection-dependecy/src/main/java/com/springboot/injection_dependency/app/AppConfig.java
+++ springboot-injection-dependecy/src/main/java/com/springboot/injection_dependency/app/AppConfig.java
@@ -1,32 +1,25 @@
 package com.springboot.injection_dependency.app;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.springboot.injection_dependency.app.models.domain.ItemFactura;
-import com.springboot.injection_dependency.app.models.domain.Producto;
-import com.springboot.injection_dependency.app.models.service.IServicio;
-import com.springboot.injection_dependency.app.models.service.MiServicio;
-import com.springboot.injection_dependency.app.models.service.MiServicio2;
+import java.util.*;
+import org.springframework.context.annotation.*;
+import com.springboot.injection_dependency.app.models.domain.*;
+import com.springboot.injection_dependency.app.models.service.*;
 
 @Configuration
 public class AppConfig {
 
     @Bean("primerServicio")
-    public IServicio registraServicio() {
+    IServicio registraServicio() {
         return new MiServicio();
     }
 
     @Bean("servicioOpcional")
-    public IServicio registraServicioComplejo() {
+    IServicio registraServicioComplejo() {
         return new MiServicio2();
     }
 
     @Bean("itemsFactura")
-    public List<ItemFactura> registrarItems() {
+    List<ItemFactura> registrarItems() {
         Producto producto1 = new Producto("Consola PS5", 499);
         Producto producto2 = new Producto("Volskwagen Passat", (int) 10.000);
 
@@ -37,7 +30,7 @@ public class AppConfig {
     }
 
     @Bean("itemsFacturaOficina")
-    public List<ItemFactura> registrarItemsOficina() {
+    List<ItemFactura> registrarItemsOficina() {
         Producto producto1 = new Producto("Monitor LG", 499);
         Producto producto2 = new Producto("Portatil MSI", (int) 1.200);
         Producto producto3 = new Producto("Mesa Escritorio Gamming", 180);
